a96ace0d098ee1f1cbf5f6927460be8be5fcb0d3
TLAU <tlau@Jonathans-MacBook-Pro-2.local>
2019-06-11 15:18:45 -0700

    Fix App.js

--- client/App.js
+++ client/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import RootStackNavigator from "./js/navigation/RootStackNavigator";
-import SurveyDifficultyContainer from "./js/screens/Lessons/SurveyDifficulty/SurveydifficultyContainer";
 export default class App extends Component {
   render() {
-    // return <RootStackNavigator />;
-    return <SurveyDifficultyContainer/>
+    return <RootStackNavigator />;
   }
 }
