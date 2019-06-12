fd02fd49e2aa79fbd7a60023b7d4c4c628fbd8f5
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-12 11:05:02 -0700

    Add apollo client provider

--- client/App.js
+++ client/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import { View } from "react-native";
 import RootStackNavigator from "./js/navigation/RootStackNavigator";
+import { ApolloProvider } from "react-apollo";
+import client from "./config/api";
 export default class App extends Component {
   render() {
-    return <RootStackNavigator />;
+    return (
+      <ApolloProvider client={client}>
+        <RootStackNavigator />
+      </ApolloProvider>
+    );
   }
 }
