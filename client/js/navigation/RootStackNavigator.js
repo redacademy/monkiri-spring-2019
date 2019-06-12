bbe9f029be3fd30b269b16b1d5898a65019f0eeb
Frank Fang <frankfang2014@hotmail.com>
2019-06-12 15:26:36 -0700

    clean comments

--- client/js/navigation/RootStackNavigator.js
+++ client/js/navigation/RootStackNavigator.js
@@ -5,14 +5,7 @@ import {
 } from "react-navigation";
 import AuthScreen from "../screens/SignIn";
 import NavigationLayout from "./NavigationLayout";
-// export default createAppContainer(
-//   createStackNavigator(
-//     { Layout: NavigationLayout },
-//     {
-//       headerMode: "none"
-//     }
-//   )
-// );
+
 const AuthStack = createStackNavigator({ SignIn: AuthScreen });
 export default createAppContainer(
   createSwitchNavigator(
