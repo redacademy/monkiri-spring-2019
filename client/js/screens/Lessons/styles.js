01dcc7a289a8e2db0ff09d66d62a3ec0973acc31
Saveen Toor <toorsaveen@gmail.com>
2019-06-12 14:03:18 -0700

    Add image icons

--- client/js/screens/Lessons/styles.js
+++ client/js/screens/Lessons/styles.js
@@ -1,3 +1,10 @@
 import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  iconContainer: {
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
