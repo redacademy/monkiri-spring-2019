ffc865d1547b27bffc695bcc6c25d8d9fe551f0f
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-13 11:35:37 -0700

    Component for login and signup forms

---
+++ client/js/components/AccountForm/AccountForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+import { styles } from "./styles";
+const AccountForm = () => {
+  return <Text>This is the account form</Text>;
+};
+
+export default AccountForm;
