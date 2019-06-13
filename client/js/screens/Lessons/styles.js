e5a26af6bcad91f01b13d0e20338c9a11ed11c2a
Saveen Toor <toorsaveen@gmail.com>
2019-06-13 09:29:15 -0700

    Edit card styling

--- client/js/screens/Lessons/styles.js
+++ client/js/screens/Lessons/styles.js
@@ -3,8 +3,27 @@ export const styles = StyleSheet.create({
   iconContainer: {
     width: "20%",
     aspectRatio: 1,
-    borderRadius: 10,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    shadowColor: "red",
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    shadowOffset: { height: 5, width: 0 }
+  },
+  name: {
+    textAlign: "center",
+    
+    
+  },
+  cardContainer: {
+    backgroundColor: "white",
+    width: "50%"
   }
+
 });
