51bf0043984a0719e2cf22709bce56d2cb1169cb
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-13 18:37:19 -0700

    Add markup for signin screen

--- client/js/screens/SignIn/SignIn.js
+++ client/js/screens/SignIn/SignIn.js
@@ -1,12 +1,92 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Image,
+  TouchableHighlight,
+  TextInput,
+  Text,
+  KeyboardAvoidingView
+} from "react-native";
 import { styles } from "./styles";
+import PropTypes from "prop-types";
+import { Form, Field } from "react-final-form";
+
 const SignIn = ({ navigation }) => {
   return (
-    <View style={styles.SignInContainer}>
-      <Button title="Sign In" onPress={() => navigation.navigate("Layout")} />
+    <View style={styles.root}>
+      <Image
+        style={styles.logo}
+        source={require("../../assets/images/Logo.png")}
+      />
+      <Form
+        onSubmit={values => {
+          console.log(values);
+          navigation.navigate("Layout");
+        }}
+        render={({ handleSubmit }) => (
+          <KeyboardAvoidingView>
+            <Field
+              name="name"
+              render={({ input, meta }) => (
+                <TextInput
+                  {...input}
+                  placeholder="Name"
+                  keyboardType="default"
+                  value={input.value}
+                />
+              )}
+            />
+            <Field
+              name="email"
+              render={({ input, meta }) => (
+                <TextInput
+                  {...input}
+                  placeholder="Email"
+                  keyboardType="default"
+                  value={input.value}
+                />
+              )}
+            />
+            <Field
+              name="password"
+              render={({ input, meta }) => (
+                <TextInput
+                  {...input}
+                  placeholder="Password"
+                  keyboardType="default"
+                  secureTextEntry={true}
+                  value={input.value}
+                />
+              )}
+            />
+            <TouchableHighlight onPress={handleSubmit}>
+              <Text> Sign In</Text>
+            </TouchableHighlight>
+            <Text>Forgot Password?</Text>
+          </KeyboardAvoidingView>
+        )}
+      />
+      <View>
+        <View>
+          <Text>Or Sign in with:</Text>
+        </View>
+        <View>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/facebook.png")}
+          />
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/google.png")}
+          />
+        </View>
+      </View>
     </View>
   );
+};
+
+SignIn.proptypes = {
+  navigation: PropTypes.array.isRequired
 };
 
 export default SignIn;
