a29a78d9b536fa5d77960d36d3cf88391d956e0f
Naiji Zhang <naijizhang@gmail.com>
2019-06-13 21:15:31 -0700

    Add testing topics data file

---
+++ client/js/screens/Lessons/SurveyLessons/helper/data.js
@@ -0,0 +1,49 @@
+import theme from "../../../../config/styles";
+export default (topics = [
+  {
+    name: "Saving",
+    icon: require("../../../../assets/images/outlinedIcons/saving.png"),
+    background: theme.colors.vitalGreen
+  },
+  {
+    name: "Budgeting",
+    icon: require("../../../../assets/images/outlinedIcons/budgeting.png"),
+    background: theme.colors.springGreen
+  },
+  {
+    name: "Investment",
+    icon: require("../../../../assets/images/outlinedIcons/investment.png"),
+    background: theme.colors.seaBlue
+  },
+  {
+    name: "Mobile Wallet",
+    icon: require("../../../../assets/images/outlinedIcons/mobilewallet.png"),
+    background: theme.colors.sunYellow
+  },
+  {
+    name: "Taxes",
+    icon: require("../../../../assets/images/outlinedIcons/taxes.png"),
+    background: theme.colors.cherryRed
+  },
+  {
+    name: "Debt",
+    icon: require("../../../../assets/images/outlinedIcons/debt.png"),
+    background: theme.colors.salmonPink
+  },
+  {
+    name: "Insurance",
+    icon: require("../../../../assets/images/outlinedIcons/insurance.png"),
+    background: theme.colors.activeOrange
+  },
+  {
+    name: "Business",
+    icon: require("../../../../assets/images/outlinedIcons/business.png"),
+    background: theme.colors.unitedNationBlue
+  },
+  {
+    name: "International Money",
+    icon: require("../../../../assets/images/outlinedIcons/internationmoney.png"),
+    background: theme.colors.lavender
+  }
+]);
+ßß;
