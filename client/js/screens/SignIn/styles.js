c7194981214c249b4e32f546895ea98c330864f0
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-13 20:05:10 -0700

    Style screen

--- client/js/screens/SignIn/styles.js
+++ client/js/screens/SignIn/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import theme from "../../config/styles";
 
 export const styles = StyleSheet.create({
   root: {
@@ -8,10 +9,56 @@ export const styles = StyleSheet.create({
   },
   logo: {
     height: 80,
-    width: 80
+    width: 80,
+    marginTop: 200,
+    marginBottom: 20
+  },
+  field: {
+    marginBottom: 8,
+    marginTop: 8
+  },
+  input: {
+    height: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 10,
+    backgroundColor: theme.colors.activeOrange
+  },
+  error: {
+    color: "red"
+  },
+  btn: {
+    marginBottom: 20,
+    marginTop: 20,
+    width: 200,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.monikOrange,
+    borderRadius: 20
+  },
+  btnText: {
+    textTransform: "capitalize",
+    color: "white"
+  },
+  forgot: {
+    marginBottom: 20,
+    marginTop: 20,
+    textDecorationLine: "underline"
+  },
+  text: {
+    marginBottom: 10,
+    marginTop: 10
+  },
+  iconsContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: "row"
   },
   icon: {
-    height: 70,
-    width: 70
+    height: 40,
+    width: 40,
+    marginLeft: 20,
+    marginRight: 20
   }
 });
