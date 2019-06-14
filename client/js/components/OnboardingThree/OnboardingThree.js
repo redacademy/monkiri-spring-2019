71e18eb3da656f2d2a5094dc4c62a4689658d885
Frank Fang <frankfang2014@hotmail.com>
2019-06-14 13:40:15 -0700

    Add toggle button

--- client/js/components/OnboardingThree/OnboardingThree.js
+++ client/js/components/OnboardingThree/OnboardingThree.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 const OnboardingThree = () => {
+  const [isRegisterActive, setIsRegisterActive] = useState(false);
+  const [isSignInActive, setIsSignInActive] = useState(false);
+
+  const handleReisterToggle = () => {
+    setIsRegisterActive(!isRegisterActive);
+    setIsSignInActive(false);
+  };
+  const handleSignInToggle = () => {
+    setIsSignInActive(!isSignInActive);
+    setIsRegisterActive(false);
+  };
+
   return (
-    <View>
-      <Text>Onboarding 03</Text>
+    <View style={styles.root}>
+      <Image
+        style={styles.logo}
+        source={require("../../assets/images/onBoarding/logo.png")}
+      />
+      <Text style={styles.subtitle}>Start Growing</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={isRegisterActive ? styles.activeButton : styles.button}
+          onPress={handleReisterToggle}
+        >
+          <Text
+            style={
+              isRegisterActive ? styles.activeButtonText : styles.buttonText
+            }
+          >
+            Register
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={isSignInActive ? styles.activeButton : styles.button}
+          onPress={handleSignInToggle}
+        >
+          <Text
+            style={isSignInActive ? styles.activeButtonText : styles.buttonText}
+          >
+            Sign In
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View />
     </View>
   );
 };
--- client/js/components/OnboardingThree/styles.js
+++ client/js/components/OnboardingThree/styles.js
@@ -1,3 +1,59 @@
 import { StyleSheet } from "react-native";
+import theme from "../../config/styles";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center"
+  },
+  logo: {
+    resizeMode: "contain",
+    width: "100%",
+    maxWidth: 300,
+    marginTop: "20%",
+    marginBottom: "10%"
+  },
+  subtitle: {
+    color: theme.colors.skyBlue,
+    fontWeight: theme.fonts.subheading.fontWeight,
+    fontSize: theme.fonts.subheading.fontSize * 1.2,
+    letterSpacing: 3
+  },
+  buttonContainer: {
+    marginTop: "10%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    height: "30%"
+  },
+  activeButton: {
+    backgroundColor: theme.colors.skyBlue,
+    width: "80%",
+    height: "auto",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10
+  },
+  activeButtonText: {
+    fontSize: theme.fonts.subheading2.fontSize,
+    fontWeight: theme.fonts.subheading2.fontWeight,
+    color: "#FFFFFF"
+  },
+  button: {
+    backgroundColor: "#ffffff",
+    width: "80%",
+    height: "auto",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: theme.colors.skyBlue
+  },
+  buttonText: {
+    fontSize: theme.fonts.subheading2.fontSize,
+    fontWeight: theme.fonts.subheading2.fontWeight,
+    color: theme.colors.skyBlue
+  }
+});
