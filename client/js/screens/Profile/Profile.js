a7d80272d31f0857f80077eca6db5835138b6ddc
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-17 16:01:03 -0700

    Add logout button to screen

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -1,7 +1,25 @@
 import React from "react";
-import { View } from "react-native";
-const Profile = () => {
-  return <View />;
+import { View, Text, TouchableOpacity } from "react-native";
+import { logOut } from "../../config/models";
+import PropTypes from "prop-types";
+
+const Profile = ({ navigation }) => {
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          logOut();
+          navigation.navigate("Auth");
+        }}
+      >
+        <Text>Log Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+Profile.proptypes = {
+  navigation: PropTypes.array.isRequired
 };
 
 export default Profile;
--- client/js/screens/Profile/ProfileContainer.js
+++ client/js/screens/Profile/ProfileContainer.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import Profile from "./Profile";
 
 class ProfileContainer extends Component {
   static navigationOptions = {
     title: "PROFILE"
   };
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Profile Screen</Text>
-      </View>
-    );
+    return <Profile navigation={this.props.navigation} />;
   }
 }
 
