2a591afff7fc5c9d3380e9d001293f7fe8079945
Naiji Zhang <naijizhang@gmail.com>
2019-06-18 15:21:41 -0700

    Install react-native-color-matrix-image-filters

--- client/android/app/src/main/java/com/monkirispring2019/MainApplication.java
+++ client/android/app/src/main/java/com/monkirispring2019/MainApplication.java
@@ -3,6 +3,7 @@ package com.monkirispring2019;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import iyegoroff.RNColorMatrixImageFilters.ColorMatrixImageFiltersPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ColorMatrixImageFiltersPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new AsyncStoragePackage()
