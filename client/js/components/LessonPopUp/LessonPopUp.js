ac0f6cc576b12f03fa8ced62fa9735b4d937626c
Naiji Zhang <naijizhang@gmail.com>
2019-06-18 16:06:51 -0700

    Add LessonPopUp component

---
+++ client/js/components/LessonPopUp/LessonPopUp.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Button, Text, View, TouchableOpacity, Image } from "react-native";
+import Modal from "react-native-modal";
+import styles from "./styles";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default class LessonPopUp extends Component {
+  state = {
+    isModalVisible: false
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
+  render() {
+    const { topic } = this.props;
+    return (
+      <View>
+        <Button title="Show modal" onPress={this.toggleModal} />
+        <Modal isVisible={this.state.isModalVisible}>
+          <View style={styles.root}>
+            <View
+              style={[
+                { backgroundColor: topic.background },
+                styles.iconContainer
+              ]}
+            >
+              <Image style={styles.icon} source={topic.icon} />
+            </View>
+            <View style={styles.textContainer}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>{topic.name}</Text>
+              </View>
+              <Text style={styles.description}>
+                {topic.description
+                  ? topic.description
+                  : "There is no description for this lesson."}
+              </Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+              <TouchableOpacity
+                style={[styles.addButtonContainer, styles.burronShadow]}
+                onPress={this.toggleModal}
+              >
+                <Text style={styles.addButtonText}>Add to lessons</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.quizButtonContainer, styles.burronShadow]}
+                onPress={this.toggleModal}
+              >
+                <Text style={styles.quizButtonText}>Test your knowledge</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity
+              style={styles.closeButtonContainer}
+              onPress={this.toggleModal}
+            >
+              <Ionicons size={25} style={styles.closeIcon} name="ios-close" />
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
