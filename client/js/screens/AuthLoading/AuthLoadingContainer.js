5b1e80907a0523d4fe13b4c79ca6a9d75eb47479
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-18 09:49:25 -0700

    Refactor code

--- client/js/screens/AuthLoading/AuthLoadingContainer.js
+++ client/js/screens/AuthLoading/AuthLoadingContainer.js
@@ -3,23 +3,11 @@ import AuthLoading from "./AuthLoading";
 import { getUserToken } from "../../config/models";
 
 class AuthLoadingContainer extends Component {
-  static navigationOptions = {
-    title: "AuthLoading"
-  };
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    this.route();
-  }
-
-  route = async () => {
+  async componentDidMount() {
     const { navigation } = this.props;
     const userToken = await getUserToken();
     navigation.navigate(userToken ? "App" : "Auth");
-  };
+  }
 
   render() {
     return <AuthLoading />;
