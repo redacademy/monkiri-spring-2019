5aa0dbf4a17384e7cd4b0f574eacf0448d27e10a
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-19 15:33:24 -0700

    Markup for screen

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -3,11 +3,18 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import { logOut } from "../../config/models";
 import PropTypes from "prop-types";
 import { styles } from "./styles";
+import theme from "../../config/styles";
+import * as Progress from "react-native-progress";
 
 const Profile = ({ navigation, user }) => {
+  const maxXp = 4000;
+  const currentXp = 600;
   return (
     <View style={styles.root}>
       <View style={styles.userInfoContainer}>
+        <TouchableOpacity style={styles.editButton}>
+          <Text style={styles.editButtonText}>Edit</Text>
+        </TouchableOpacity>
         <Image
           style={styles.backgroundImage}
           source={require("../../assets/images/profile-background.png")}
@@ -19,7 +26,37 @@ const Profile = ({ navigation, user }) => {
         </View>
       </View>
       <View style={styles.userStatsContainer}>
-        <Text>This is the user stats section</Text>
+        <View style={styles.userStatsHeaderContainer}>
+          <Text style={styles.userStatsHeader}>Level 1: NoviceLearner</Text>
+          <Image
+            style={styles.headerImage}
+            source={require("../../assets/images/trophies-banner.png")}
+          />
+        </View>
+
+        <View style={styles.userProgressContainer}>
+          <Text style={styles.userProgressText}>
+            {maxXp - currentXp} xp until you react the next level!
+          </Text>
+          <Progress.Bar
+            progress={currentXp / maxXp}
+            style={styles.progressBar}
+            width={200}
+            height={16}
+            color={theme.colors.seaBlue}
+            borderRadius={10}
+          />
+          <Text style={styles.userProgressText2}>
+            {currentXp}/{maxXp}xp
+          </Text>
+        </View>
+        <View style={styles.userStatsLessonsContainer}>
+          <Text style={styles.userStatsLessonsText}>Lessons Completed:</Text>
+          <Text style={styles.userStatsLessons}>0</Text>
+        </View>
+        <TouchableOpacity style={styles.shareButton}>
+          <Text style={styles.shareButtonText}>Share your stats</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.signOutContainer}>
         <TouchableOpacity
@@ -32,7 +69,7 @@ const Profile = ({ navigation, user }) => {
             }
           }}
         >
-          <Text>Sign Out</Text>
+          <Text style={styles.signOut}>Sign Out</Text>
         </TouchableOpacity>
       </View>
     </View>
