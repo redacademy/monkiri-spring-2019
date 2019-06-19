9b93a2ea3f1296e04a0da101ba13c101ed1025a7
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-19 13:39:22 -0700

    Fetch user and pass it as a prop

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -2,28 +2,33 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { logOut } from "../../config/models";
 import PropTypes from "prop-types";
+import { styles } from "./styles";
 
-const Profile = ({ navigation }) => {
+const Profile = ({ navigation, user }) => {
+  console.log(user);
   return (
     <View>
-      <TouchableOpacity
-        onPress={async () => {
-          try {
-            logOut();
-            navigation.navigate("Auth");
-          } catch (e) {
-            return false;
-          }
-        }}
-      >
-        <Text>Log Out</Text>
-      </TouchableOpacity>
+      <View>
+        <TouchableOpacity
+          onPress={async () => {
+            try {
+              logOut();
+              navigation.navigate("Auth");
+            } catch (e) {
+              return false;
+            }
+          }}
+        >
+          <Text>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 Profile.proptypes = {
-  navigation: PropTypes.array.isRequired
+  navigation: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired
 };
 
 export default Profile;
