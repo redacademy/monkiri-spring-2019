0ec6948541fb07cf277b85ac91feb13ad3175e01
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-19 09:45:45 -0700

    Apply font styling

--- client/js/components/Loader/styles.js
+++ client/js/components/Loader/styles.js
@@ -1,9 +1,12 @@
 import { StyleSheet } from "react-native";
-
+import theme from "../../config/styles";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  text: {
+    ...theme.fonts.subheading2
   }
 });
