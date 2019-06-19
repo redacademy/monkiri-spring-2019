622800c97efc77c2a2fcec3ed77b2f5406cd7877
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-19 14:14:23 -0700

    Markup for user info

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { logOut } from "../../config/models";
 import PropTypes from "prop-types";
 import { styles } from "./styles";
 
 const Profile = ({ navigation, user }) => {
-  console.log(user);
   return (
-    <View>
-      <View>
+    <View style={styles.root}>
+      <View style={styles.userInfoContainer}>
+        <Image
+          style={styles.backgroundImage}
+          source={require("../../assets/images/profile-background.png")}
+        />
+        <Image style={styles.userAvatar} source={{ uri: user.image }} />
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>{user.name}</Text>
+          <Text style={styles.userEmail}>{user.email}</Text>
+        </View>
+      </View>
+      <View style={styles.userStatsContainer}>
+        <Text>This is the user stats section</Text>
+      </View>
+      <View style={styles.signOutContainer}>
         <TouchableOpacity
           onPress={async () => {
             try {
