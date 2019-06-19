6c7332976bfe35055ff2b1d12e5c1afffb1830e0
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-19 15:33:46 -0700

    Style screen

--- client/js/screens/Profile/styles.js
+++ client/js/screens/Profile/styles.js
@@ -13,6 +13,21 @@ export const styles = StyleSheet.create({
     flex: 2,
     position: "relative"
   },
+  editButton: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    zIndex: 5,
+    backgroundColor: theme.colors.skyBlue,
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 25
+  },
+  editButtonText: {
+    ...theme.fonts.subheading2,
+    color: "white",
+    textTransform: "uppercase"
+  },
   backgroundImage: {
     resizeMode: "cover"
   },
@@ -36,9 +51,78 @@ export const styles = StyleSheet.create({
     ...theme.fonts.bodytext
   },
   userStatsContainer: {
-    flex: 2
+    flex: 2,
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  userStatsHeaderContainer: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+    width: width * 0.7
+  },
+  headerImage: {
+    resizeMode: "stretch",
+    position: "absolute",
+    bottom: 0,
+    zIndex: -5
+  },
+  userStatsHeader: {
+    ...theme.fonts.subheading,
+    color: "white",
+    backgroundColor: theme.colors.skyBlue,
+    paddingVertical: 5,
+    textAlign: "center",
+    width: "100%"
+  },
+  userProgressContainer: {
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  userProgressText: {
+    ...theme.fonts.bodytext
+  },
+  userProgressText2: {
+    width: "100%",
+    textAlign: "right",
+    ...theme.fonts.bodytext2
+  },
+  progressBar: {
+    marginVertical: 5,
+    backgroundColor: theme.colors.seaBlue,
+    borderColor: theme.colors.seaBlue
+  },
+  userStatsLessonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  userStatsLessonsText: {
+    ...theme.fonts.subheading2,
+    marginRight: 5
+  },
+  userStatsLessons: {
+    ...theme.fonts.heading,
+    color: theme.colors.sunYellow,
+    fontSize: theme.fonts.heading.fontSize * 2
+  },
+  shareButton: {
+    backgroundColor: theme.colors.monikOrange,
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 25,
+    marginBottom: 10
+  },
+  shareButtonText: {
+    ...theme.fonts.subheading2,
+    color: "white",
+    textTransform: "capitalize"
   },
   signOutContainer: {
     flex: 0.25
+  },
+  signOut: {
+    textDecorationLine: "underline",
+    ...theme.fonts.bodytext2
   }
 });
