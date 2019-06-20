00c53098f59a422232539449ecbe6fa5ff6ca366
Frank Fang <frankfang2014@hotmail.com>
2019-06-20 15:26:41 -0700

    Swich back

--- client/js/screens/Lessons/LessonsContainer.js
+++ client/js/screens/Lessons/LessonsContainer.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import Calculator from "../../components/Calculator";
 class LessonsContainer extends Component {
   static navigationOptions = {
     title: "LESSONS"
   };
   render() {
     return (
-      // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      //   <Text>Lessons Screen</Text>
-      // </View>
-      <Calculator />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Lessons Screen</Text>
+      </View>
     );
   }
 }
