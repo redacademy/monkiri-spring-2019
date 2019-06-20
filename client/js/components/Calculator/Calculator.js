8a290a203dd93ad63128b0ac7b25c78db568434d
Frank Fang <frankfang2014@hotmail.com>
2019-06-20 15:15:17 -0700

    Add calculator

--- client/js/components/Calculator/Calculator.js
+++ client/js/components/Calculator/Calculator.js
@@ -1,47 +1,92 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, Picker } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Picker,
+  TouchableOpacity,
+  Slider,
+  ScrollView
+} from "react-native";
+import theme from "../../config/styles";
 import { styles } from "./styles";
 const Calculator = () => {
-  const [principal, setPrincipal] = useState(0);
-  const [interestRate, setInterestRate] = useState(0.01);
-  const [period, setPeriod] = useState(6);
-  const [time, setTime] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [principal, setPrincipal] = useState("");
+  const [interestRate, setInterestRate] = useState("");
+  const [period, setPeriod] = useState(4);
+  const [time, setTime] = useState(1);
+  const [total, setTotal] = useState("");
+  const [isSumbit, setIsSubmit] = useState(false);
+  const focusRef = useRef();
+  const scrollRef = useRef();
 
+  const updateTime = time => {
+    const intTime = Math.floor(time);
+    setTime(intTime);
+  };
   const updateInterest = interest => {
-    const interestConvert = interest * 0.01;
-    setInterestRate(interestConvert);
+    setInterestRate(interest);
   };
   const updatePrincipal = principal => {
-    console.log(principal);
     setPrincipal(principal);
   };
   const updatePeriod = period => {
-    console.log(period);
     setPeriod(period);
   };
+  const calculateInterest = (principal, interestRate, period, time) => {
+    setIsSubmit(true);
+    const pow = Math.pow(1 + (interestRate * 0.01) / period, period * time);
+    const result = Number.parseFloat(principal * pow).toFixed(2);
+    setTotal(result);
+    setTimeout(() => {
+      scrollRef.current.scrollToEnd({ animated: true });
+    }, 10);
+  };
+
+  const convertPeriod = period => {
+    if (period === 1) {
+      return "Annually";
+    } else if (period === 2) {
+      return "Semi-Annually";
+    } else if (period === 4) {
+      return "Quarterly";
+    } else if (period === 6) {
+      return "Bi-Monthly";
+    } else if (period === 12) {
+      return "Monthly";
+    } else if (period === 24) {
+      return "Semi-Monthly";
+    }
+  };
   return (
-    <View>
+    <ScrollView style={styles.root} ref={scrollRef}>
       <View style={styles.principal}>
-        <Text>$</Text>
+        <Text style={styles.title}>Your Principal:</Text>
+        <Text> $ </Text>
         <TextInput
+          value={principal}
           style={styles.input}
           onChangeText={updatePrincipal}
           placeholder="Your Principal"
+          ref={focusRef}
         />
       </View>
       <View style={styles.interest}>
+        <Text style={styles.title}>Your Interest: </Text>
         <TextInput
+          value={interestRate}
           style={styles.input}
           onChangeText={updateInterest}
           placeholder="Your Interest"
         />
-        <Text>%</Text>
+        <Text> %</Text>
       </View>
-      <View>
+      <View style={styles.compound}>
+        <Text style={styles.title}>Interest is compounded: </Text>
         <Picker
           selectedValue={period}
           onValueChange={itemValue => updatePeriod(itemValue)}
+          style={styles.picker}
         >
           <Picker.Item label="Semi-Monthly(24/Yr)" value={24} />
           <Picker.Item label="Monthly(12/Yr)" value={12} />
@@ -51,7 +96,43 @@ const Calculator = () => {
           <Picker.Item label="Annually(1/Yr)" value={1} />
         </Picker>
       </View>
-    </View>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.title}>Years to grow: </Text>
+        <Text style={styles.sliderTitle}>Year:{time}</Text>
+        <View style={styles.sliderContent}>
+          <Text>0</Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={50}
+            maximumTrackTintColor={theme.colors.seaBlue}
+            onValueChange={updateTime}
+            value={time}
+          />
+          <Text>50</Text>
+        </View>
+      </View>
+      <View style={styles.result}>
+        <TouchableOpacity
+          onPress={() =>
+            calculateInterest(principal, interestRate, period, time)
+          }
+        >
+          <Text style={styles.button}>Calculate</Text>
+        </TouchableOpacity>
+        {isSumbit ? (
+          <Text style={styles.title}>
+            Your initial investment of
+            <Text style={styles.highlight}> ${principal}</Text> at an annualized
+            interest rate of
+            <Text style={styles.highlight}> {interestRate}%</Text>
+            will be worth<Text style={styles.highlight}> ${total} </Text>,after
+            <Text style={styles.highlight}> {time} </Text> years when compounded
+            <Text style={styles.highlight}> {convertPeriod(period)}</Text>.
+          </Text>
+        ) : null}
+      </View>
+    </ScrollView>
   );
 };
 
--- client/js/components/Calculator/styles.js
+++ client/js/components/Calculator/styles.js
@@ -2,13 +2,60 @@ import { StyleSheet, Dimensions } from "react-native";
 import theme from "../../config/styles";
 const { width, height } = Dimensions.get("window");
 export const styles = StyleSheet.create({
-  input: { width: 100, height: 40, borderWidth: 2 },
-  principal: {
+  input: {
+    width: width * 0.3,
+    height: 40,
+    borderWidth: 2,
+    borderColor: theme.colors.seaBlue,
+    padding: 5
+  },
+  sliderTitle: {
+    textAlign: "center"
+  },
+  highlight: {
+    color: "#ee7f33"
+  },
+  sliderContent: {
     flexDirection: "row",
     alignItems: "center"
   },
+  sliderContainer: {
+    width: width,
+    margin: 5
+  },
+  slider: {
+    width: width * 0.9,
+    height: 30
+  },
+  title: {
+    ...theme.fonts.subheading,
+    color: theme.colors.skyBlue
+  },
+  principal: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 5
+  },
   interest: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    margin: 5
+  },
+  compound: {
+    margin: 5
+  },
+  result: {
+    margin: 5
+  },
+  button: {
+    borderWidth: 3,
+    width: width * 0.4,
+    padding: 10,
+    borderColor: theme.colors.skyBlue,
+    ...theme.fonts.subheading,
+    color: "white",
+    backgroundColor: theme.colors.skyBlue,
+    textAlign: "center",
+    marginBottom: 5
   }
 });
