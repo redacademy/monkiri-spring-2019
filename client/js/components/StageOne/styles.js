2902689e3cef487daf4518fdafaffe0328a3fb6a
Saveen Toor <toorsaveen@gmail.com>
2019-06-20 16:33:12 -0700

    Edit styling for digital currency

--- client/js/components/StageOne/styles.js
+++ client/js/components/StageOne/styles.js
@@ -1,32 +1,59 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import theme from "../../config/styles";
+const { width, height } = Dimensions.get("window");
 export const styles = StyleSheet.create({
   title: {
     flex: 1,
-    alignItems: "center",
+    
     width: "100%",
-    justifyContent: "space-evenly",
-    padding: "2%"
+
+    padding: "5%"
+  },
+  content: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  digitalCurrency: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    fontSize: 16,
+    fontWeight: "600",
+    textTransform: "uppercase"
+  },
+  progressBar: {
+    marginBottom: 10
   },
   heading1: {
     color: "#25BDD8",
-    fontWeight: "600"
+    fontWeight: "600",
+    
   },
   compoundInterest: {
+    // Top: '3%',
+    marginTop: height * 0.02,
+    marginBottom: height * 0.03
+  },
+  compoundInterest1: {
     flexDirection: "row",
     flexWrap: "wrap",
-    paddingBottom: '5%'
+
+    
+  },
+  cI: {
+    marginTop: height * 0.04
   },
   simpleVsCompound: {
     color: "white",
-    backgroundColor: "#0274ff",
-    padding: 5,
-    width: "100%",
+    backgroundColor: theme.colors.skyBlue,
+    padding: 15,
+    width: width * 0.9,
     fontWeight: "600",
     fontSize: 15,
     alignItems: "center",
-    paddingBottom: '2%',
-    marginBottom: '2%'
+    paddingBottom: "2%",
+    textAlign: "center"
+    // marginBottom: '3%',
   },
   table: {
     flexDirection: "row",
@@ -49,13 +76,20 @@ export const styles = StyleSheet.create({
     borderColor: "#d6d7da"
   },
   interest: {
-    padding: 5
+    padding: 5,
+    lineHeight: 20,
   },
 
   circleOne: {
     width: "100%",
     justifyContent: "center",
     alignItems: "center"
+  },
+  circleTwo: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+    // marginTop: '5%'
   },
   circleImg: {
     width: 40,
@@ -64,16 +98,20 @@ export const styles = StyleSheet.create({
   numberOne: {
     position: "absolute",
     alignSelf: "center",
-    alignItems: "center"
+    alignItems: "center",
+    lineHeight: 20,
   },
   one: {
-    color: "#fff"
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "400"
   },
   buttonsContainer: {
-    width: "70%",
+    width: 0.7 * width,
     justifyContent: "space-between",
     marginLeft: "auto",
-    marginRight: "auto"
+    marginRight: "auto",
+    paddingTop: "5%"
   },
   orangeButtonContainer: {
     backgroundColor: theme.colors.monikOrange,
@@ -82,8 +120,6 @@ export const styles = StyleSheet.create({
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#fff",
     marginTop: 10
   },
   orangeButtonText: {
@@ -97,5 +133,15 @@ export const styles = StyleSheet.create({
     shadowRadius: 2,
     shadowOffset: { height: 3, width: 0 },
     shadowRadius: 3
+  },
+  textOne: {
+    paddingTop: "10%",
+    paddingBottom:  "10%",
+    lineHeight: 20,
+
+  },
+  textTwo: {
+    paddingTop: 15,
+    lineHeight: 20
   }
 });
