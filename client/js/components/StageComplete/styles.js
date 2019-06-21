57db3a30051c8d71f2e1e599cb1e1f1b43b594a4
Naiji Zhang <naijizhang@gmail.com>
2019-06-21 10:17:22 -0700

    Initialize StageComplete component

---
+++ client/js/components/StageComplete/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Dimensions } from "react-native";
+import theme from "../../config/styles";
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  
+});
+export default styles;
