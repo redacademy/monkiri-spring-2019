e1aa00ec105488bc87359ee9be79c758cf93f394
Frank Fang <frankfang2014@hotmail.com>
2019-06-21 10:53:09 -0700

    Move necessary images  and use linking

--- client/js/components/PartnersCardExpand/PartnersCardExpand.js
+++ client/js/components/PartnersCardExpand/PartnersCardExpand.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, Linking, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 const PartnersCardExpand = ({ data }) => {
   return data.isOpen ? (
@@ -10,7 +10,13 @@ const PartnersCardExpand = ({ data }) => {
       ]}
     >
       {data.partnersIcon.map((icon, index) => (
-        <Image key={index} source={icon} style={styles.icon} />
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL("https://www.pipay.com/");
+          }}
+        >
+          <Image key={index} source={icon} style={styles.icon} />
+        </TouchableOpacity>
       ))}
     </View>
   ) : (
