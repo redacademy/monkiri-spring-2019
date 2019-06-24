27bffa0844ffe92eb24e16280b9fd2987e9deef3
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-24 16:31:36 -0700

    Create ui for quests screen

--- client/js/screens/Quests/styles.js
+++ client/js/screens/Quests/styles.js
@@ -1,3 +1,58 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({});
+import { StyleSheet, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+export const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    alignItems: "center"
+  },
+  buttonClicked: {
+    backgroundColor: "#f07216",
+    color: "#fff",
+    height: "92%",
+    width: "100%",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#f07216",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  buttonNotClicked: {
+    backgroundColor: "#ffffff",
+    textTransform: "uppercase",
+    height: "78%",
+    width: "100%",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#f07216"
+  },
+  buttonTextNotClicked: {
+    color: "#f07216",
+    textTransform: "uppercase"
+  },
+  buttonTextClicked: {
+    color: "#fff",
+    textTransform: "uppercase"
+  },
+  tabIndex: {
+    height: 60,
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 20
+  },
+  tabLayout: {
+    flex: 0.5,
+    height: "100%",
+    justifyContent: "flex-end",
+    flexDirection: "column"
+  },
+  tabBorder: {
+    flex: 0.5,
+    height: "100%",
+    justifyContent: "flex-end",
+    flexDirection: "column"
+  }
+});
