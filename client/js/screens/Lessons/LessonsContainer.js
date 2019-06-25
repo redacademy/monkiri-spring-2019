f1987437064378bda0cae0443b76127e40682c3f
Saveen Toor <toorsaveen@gmail.com>
2019-06-25 08:54:21 -0700

    Updated delete functionality

--- client/js/screens/Lessons/LessonsContainer.js
+++ client/js/screens/Lessons/LessonsContainer.js
@@ -9,7 +9,8 @@ class LessonsContainer extends Component {
     super(props);
     this.state = {
       tabIndex: 0,
-      isEditMode: false
+      isEditMode: false,
+      selectedTopics: { Saving: true, Taxes: true }
     };
   }
 
@@ -19,21 +20,101 @@ class LessonsContainer extends Component {
   toggleEditMode() {
     this.setState({ isEditMode: !this.state.isEditMode });
   }
+  addLesson(lesson) {
+    const currentSelected = this.state.selectedTopics;
+    currentSelected[lesson] = true;
+    this.setState({ selectedTopics: currentSelected });
+  }
+  removeLesson(lesson) {
+    const currentSelected = this.state.selectedTopics;
+    currentSelected[lesson] = false;
+    this.setState({ selectedTopics: currentSelected });
+  }
 
   render() {
-    const selectedTopics = [
+    const { selectedTopics } = this.state;
+    const topics = [
       {
         name: "Saving",
         icon: require("../../assets/images/outlinedIcons/saving.png"),
         background: theme.colors.vitalGreen,
-        description: undefined
+        description: undefined,
+        stages: [
+          {
+            id: 1,
+            type: "content",
+            lessonName: "Introduction",
+            icon: require("../../assets/images/outlinedIcons/business.png"),
+            avaiable: true,
+            isCompleted: false,
+            text: "Start the Quiz to water and grow your sprout!"
+          },
+          {
+            id: 2,
+            type: "quiz",
+            lessonName: "Quiz",
+            icon: require("../../assets/images/outlinedIcons/taxes.png"),
+            isCompleted: false,
+            avaiable: false,
+            text: "Start the Calculator to water and grow your sprout!",
+            answer: true,
+            explanation:
+              "Interest is defined as the cost of borrowing money. ... Simple interest is calculated on the principal amount of a loan only."
+          },
+          {
+            id: 3,
+            type: "application",
+            lessonName: "Calculator",
+            icon: require("../../assets/images/outlinedIcons/insurance.png"),
+            isCompleted: false,
+            avaiable: false,
+            text:
+              "You've completed all the compound interest ! Your tree is full grown."
+          }
+        ]
+      },
+      {
+        name: "Budgeting",
+        icon: require("../../assets/images/outlinedIcons/budgeting.png"),
+        background: theme.colors.springGreen
+      },
+      {
+        name: "Investment",
+        icon: require("../../assets/images/outlinedIcons/investment.png"),
+        background: theme.colors.seaBlue
+      },
+      {
+        name: "Mobile Wallet",
+        icon: require("../../assets/images/outlinedIcons/mobilewallet.png"),
+        background: theme.colors.sunYellow
       },
       {
         name: "Taxes",
         icon: require("../../assets/images/outlinedIcons/taxes.png"),
         background: theme.colors.cherryRed
+      },
+      {
+        name: "Debt",
+        icon: require("../../assets/images/outlinedIcons/debt.png"),
+        background: theme.colors.salmonPink
+      },
+      {
+        name: "Insurance",
+        icon: require("../../assets/images/outlinedIcons/insurance.png"),
+        background: theme.colors.activeOrange
+      },
+      {
+        name: "Business",
+        icon: require("../../assets/images/outlinedIcons/business.png"),
+        background: theme.colors.unitedNationBlue
+      },
+      {
+        name: "International Money",
+        icon: require("../../assets/images/outlinedIcons/internationmoney.png"),
+        background: theme.colors.lavender
       }
     ];
+
     return (
       <View style={styles.screenContainer}>
         <View style={styles.tabIndex}>
@@ -83,7 +164,7 @@ class LessonsContainer extends Component {
           </View>
         </View>
         {this.state.tabIndex !== 0 ? (
-          <LessonLibrary />
+          <LessonLibrary addLesson={this.addLesson.bind(this)} />
         ) : (
           <ScrollView>
             <View style={styles.editButtonContainer}>
@@ -97,46 +178,54 @@ class LessonsContainer extends Component {
               </TouchableOpacity>
             </View>
 
-            {selectedTopics.map(item => {
-              return (
-                <View key={item.name} style={styles.container}>
-                  {this.state.isEditMode ? (
-                    <TouchableOpacity style={styles.deleteButton}>
-                      <Ionicons size={30} name="ios-close" color="#fff" />
-                    </TouchableOpacity>
-                  ) : null}
-
-                  <View
-                    style={[
-                      { backgroundColor: item.background },
-                      styles.iconContainer,
-                      this.state.isEditMode ? styles.noBorderRadius : null
-                    ]}
-                  >
-                    <Image style={styles.whiteIcon} source={item.icon} />
-                  </View>
-
-                  <View
-                    style={[
-                      styles.cardContainer,
-                      this.state.isEditMode ? styles.editModeCardWidth : null
-                    ]}
-                  >
-                    <Text style={styles.name}>{item.name}</Text>
-                    {this.state.isEditMode ? null : <View style={styles.bar} />}
-                  </View>
-                  {this.state.isEditMode ? null : (
-                    <View style={styles.button}>
-                      <TouchableOpacity>
-                        <Image
-                          style={styles.playButton}
-                          source={require(`../../assets/images/Buttons/play_button.png`)}
-                        />
+            {topics.map(item => {
+              if (selectedTopics[item.name])
+                return (
+                  <View key={item.name} style={styles.container}>
+                    {this.state.isEditMode ? (
+                      <TouchableOpacity
+                        onPress={() => {
+                          this.removeLesson(item.name);
+                        }}
+                        style={styles.deleteButton}
+                      >
+                        <Ionicons size={30} name="ios-close" color="#fff" />
                       </TouchableOpacity>
+                    ) : null}
+
+                    <View
+                      style={[
+                        { backgroundColor: item.background },
+                        styles.iconContainer,
+                        this.state.isEditMode ? styles.noBorderRadius : null
+                      ]}
+                    >
+                      <Image style={styles.whiteIcon} source={item.icon} />
                     </View>
-                  )}
-                </View>
-              );
+
+                    <View
+                      style={[
+                        styles.cardContainer,
+                        this.state.isEditMode ? styles.editModeCardWidth : null
+                      ]}
+                    >
+                      <Text style={styles.name}>{item.name}</Text>
+                      {this.state.isEditMode ? null : (
+                        <View style={styles.bar} />
+                      )}
+                    </View>
+                    {this.state.isEditMode ? null : (
+                      <View style={styles.button}>
+                        <TouchableOpacity>
+                          <Image
+                            style={styles.playButton}
+                            source={require(`../../assets/images/Buttons/play_button.png`)}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    )}
+                  </View>
+                );
             })}
           </ScrollView>
         )}
